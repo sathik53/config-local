8e186aaccb2d8dee0c7a23ce80a4907830bf0cd2
sathiraju <sathiraju.kk@gmail.com>
2020-09-04 21:52:30 +0530

    config local app

---
+++ src/main/java/com/satya/demo/ConfigLocalApplication.java
@@ -0,0 +1,13 @@
+package com.satya.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigLocalApplication.class, args);
+	}
+
+}
